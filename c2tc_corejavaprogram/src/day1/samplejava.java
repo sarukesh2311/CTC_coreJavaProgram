1045c9d63660b67d02a1f5d37665778a33c7fba4
ACT-C2-22 <ACT-C2-22@DESKTOP-I4SCAC5>
2025-06-13 09:29:10 +0530

    this is addition

--- c2tc_corejavaprogram/src/day1/samplejava.java
+++ c2tc_corejavaprogram/src/day1/samplejava.java
@@ -1,10 +1,25 @@
 package day1;
+import java.util.Scanner;
 
-public class samplejava {
+public class addition {
 
 	public static void main(String[] args) {
-		System.out.println("welcome to the java session... ");
+		int a,b,c;
+		
+		Scanner sc;
+		sc = new Scanner(System.in);
+		
+		System.out.println("Enter a :");
+		a = sc.nextInt();
+		
+		System.out.println("Enter b : ");
+		b=sc.nextInt();
+		c = a+b;
+		System.out.println("addition of a ="+a+" and b= "+b+" is "+c);
+	
+	    sc.close();
 
 	}
-
+      
 }
+ 																																																																																		
