b4ca633d2648a67a88cf14cfde39cf0dbc362154
ACT-C2-22 <ACT-C2-22@DESKTOP-I4SCAC5>
2025-06-16 13:47:04 +0530

    upload

---
+++ c2tc_corejavaprogram/src/Demo/Student.java
@@ -0,0 +1,16 @@
+package Demo;
+
+public class StudentJava {
+
+	public static void main(String[] args) {
+		Student st =new Student ();
+		st.setId( 101);
+		st.setName ("mani");
+		st.setAge  (20);
+		System.out.println("ID:"+st.getId());
+		System.out.println("Name:"+st.getName());
+		System.out.println("Age:"+st.getAge();)
+	}
+
+}
+
